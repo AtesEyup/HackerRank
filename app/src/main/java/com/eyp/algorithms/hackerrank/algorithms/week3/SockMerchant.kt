a65876b0bd729291936a82663523fe57b7d5709f
Eyüp ATEŞ <122296204+AtesEyup@users.noreply.github.com>
2025-09-06 13:16:46 +0300

    sockMerchant çözümü

--- app/src/main/java/com/eyp/algorithms/hackerrank/algorithms/week3/SockMerchant.kt
+++ app/src/main/java/com/eyp/algorithms/hackerrank/algorithms/week3/SockMerchant.kt
@@ -3,9 +3,7 @@ package com.eyp.algorithms.hackerrank.algorithms.week3
 fun sockMerchant(n: Int, ar: Array<Int>): Int {
     var result = 0
 
-    var ss = mutableMapOf<Int, Boolean>()
-
-    var sockMap = mutableMapOf<Int, Int>()
+    val sockMap = mutableMapOf<Int, Int>()
 
     for (sock in ar) {
         sockMap[sock] = sockMap.getOrDefault(sock, 0) + 1
